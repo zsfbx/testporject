e6923281768f72b9901d54b9eca66237fa025a96
lrh <2369436123@qq.com>
2020-09-11 11:27:01 +0800

    初始化提交

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @program: testgit
+ * @description:
+ * @author: lrh
+ * @create: 2020-09-11 11:23:03
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
